97762e1099a0576a6c01fab40096216782494e12
Ruvah <39217365+Ruvah@users.noreply.github.com>
2019-10-05 23:16:22 +0200

    vertical table start

--- Assets/_Whatsup/Scripts/Texting/UIMessengerTextsManager.cs
+++ Assets/_Whatsup/Scripts/Texting/UIMessengerTextsManager.cs
@@ -26,5 +26,7 @@ public class UIMessengerTextsManager : UIObject
 
         text_message.Message = message;
     }
+
+    
     
 }
---
+++ Assets/_Whatsup/Scripts/WindowSystem/UIVerticalTable.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class UIVerticalTable : UIObject
+{
+    public enum VerticalTableAlignment
+    {
+        Centre,
+        Left,
+        Right,
+        Count
+    }
+    
+    // -- FIELDS
+
+
+    public float Spacing
+    {
+        get => _Spacing;
+        set
+        {
+            _Spacing = value;
+            RecreateTable();
+        }
+    }
+
+    private float _Spacing;
+    private List<KeyValuePair<UIObject, VerticalTableAlignment>> ItemsList;
+
+    [SerializeField] private VerticalTableAlignment ItemAlignment;
+
+    // -- METHODS
+
+    public void AddItem(UIObject item)
+    {
+        AddItem(item, ItemAlignment);
+    }
+
+    public virtual void AddItem(UIObject item, VerticalTableAlignment alignment)
+    {
+        var last_item = ItemsList.Last();
+        Vector2 anchored_position = ContentRect.max;
+    }
+    
+    private void RecreateTable()
+    {
+        
+    }
+    
+    // -- UNITY
+}
