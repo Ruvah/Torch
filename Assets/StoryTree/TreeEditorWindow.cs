7fb01fb956b7ade9cf91aa7e6edcfe70a34585a9
Ruvah <39217365+Ruvah@users.noreply.github.com>
2019-10-06 14:35:09 +0200

    story tree creation files

---
+++ Assets/StoryTree/TreeEditorWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEditor;
+
+public class TreeEditorWindow : EditorWindow
+{
+    // -- METHODS
+    
+    
+    [MenuItem("Tools/TreeEditor")]
+    static void Init()
+    {
+        TreeEditorWindow window = (TreeEditorWindow)EditorWindow.GetWindow(typeof(TreeEditorWindow));
+        window.Show();
+    }
+    
+    // -- UNITY
+
+    private void OnGUI()
+    {
+        
+    }
+}
