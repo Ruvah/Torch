c0ee2060d8af58d6f201154c0a7064aa06c2861b
Ruvah <39217365+Ruvah@users.noreply.github.com>
2019-10-06 13:08:59 +0200

    Interactable System

---
+++ Assets/_Whatsup/Scripts/CommonCode/InteractableSystem/Interactable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // -- FIELDS
+
+
+    private Outcome[] Outcomes;
+    
+    // -- METHODS
+
+    public virtual void Interact()
+    {
+        foreach (var outcome in Outcomes)
+        {
+            outcome.SetActive();
+        }
+    }
+    
+    // -- UNITY
+
+    protected void Awake()
+    {
+        Outcomes = GetComponents<Outcome>();
+    }
+}
