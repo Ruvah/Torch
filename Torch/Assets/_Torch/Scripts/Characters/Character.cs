a10b8771d73b227934ab44038e434cbcdec2a3cd
Ruvah <39217365+Ruvah@users.noreply.github.com>
2020-01-18 22:16:50 +0100

    foldure structure

---
+++ Torch/Assets/_Torch/Scripts/Characters/Character.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+   // -- PROPERTIES
+
+   public CharacterStats CharacterStats => characterStats;
+   public CharacterMovement Movement => movement;
+   public CharacterCombat Combat => combat;
+   public CharacterAnimation Animation => animation;
+
+   // -- FIELDS
+
+   [SerializeField] private CharacterStats characterStats;
+
+   [SerializeField] private CharacterMovement movement;
+   [SerializeField] private CharacterCombat combat;
+   [SerializeField] private CharacterAnimation animation;
+   [SerializeField] private CharacterHealth health;
+
+   // -- UNITY
+
+   private void Awake()
+   {
+
+      combat.Controller = this;
+      animation.Character = this;
+   }
+}
--- Torch/Assets/_Torch/Scripts/Texting/TextMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TextMessage
-{
-    public string Text;
-    public Profile Author = new Profile();
-}
